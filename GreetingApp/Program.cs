a1019d037ee8e09cce617cc05cc282646464e04e
Aguilar.304743 <Aguilar.304743@LAB-A-PC34>
2024-02-08 08:01:28 +0800

    Add project files.

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the total number of your enrolled courses: ");
+            int Courses;
+            Courses = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the price of your favorite book: ");
+            Double Price;
+            Price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Total of enrolled courses: " + Courses);
+            Console.WriteLine("Price of my favorite book " + Price);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
